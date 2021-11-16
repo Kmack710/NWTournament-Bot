05b904df366e596cc805cdec49fd0e8aa3b6ed5d
Kmack710 <69439573+Kmack710@users.noreply.github.com>
2021-11-16 14:50:36 -0800

    Auth Example

---
+++ auth - EXAMPLE.py
@@ -0,0 +1,3 @@
+import mysql.connector as mariadb 
+token = ""  ## BOT TOKEN 
+mariadb_connection = mariadb.connect(user= '', password= '', database= '', host= '', port= '3306') ### Database Connection
